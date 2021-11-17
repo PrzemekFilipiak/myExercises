4e75ff3bfcb20fc72c8332a5c535adb04fd77dd7
Przemyslaw <brain667@gmail.com>
2021-11-17 15:04:48 +0100

    Exercises 6 Alphabet

---
+++ Trainings/src/TwentySecond/Ex6_Alphabet.java
@@ -0,0 +1,18 @@
+package TwentySecond;
+
+public class Ex6_Alphabet {
+    public static void main(String[] args) {
+        for ( char i = 'a'; i <= 'z'; i++) {
+            System.out.println(i);
+        }
+
+        for ( char k = 'z'; k >= 'a'; k--) {
+            System.out.println(k);
+        }
+
+        for ( char i = 'A'; i <= 'Z'; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
